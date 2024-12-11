d752b4d11d24c8af193b0212a675ad3261cf15f9
Kaye Gamana <kayecgamana@gmail.com>
2024-12-12 06:39:24 +0800

    fixed font style

--- A08/index.php
+++ A08/index.php
@@ -28,8 +28,8 @@ while ($personalityRow = mysqli_fetch_assoc($personalitiesResult)) {
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
-  
+  <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat"> -->
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="assets/css/styles.css">
   <link rel="icon" href="assets/images/pageIcon.png" type="image/png">
